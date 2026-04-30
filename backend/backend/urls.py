ded0cf461f2fcf4260551bc0a7584d12328cb965
antonyvenis <antonyvenis1212@gmail.com>
2026-04-30 14:40:55 +0530

    e-commerce-website-food-changes-new-one

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import HttpResponse
 
 def home(request):
     return HttpResponse("Backend Running Successfully 🚀")
