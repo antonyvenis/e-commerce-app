0a4d5a6227e9012c17adaf7269adc833e6e647c0
antonyvenis <antonyvenis1212@gmail.com>
2026-05-01 16:18:36 +0530

    e-commerce-website-settings-updated

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -157,10 +157,10 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-# EMAIL_HOST_USER = 'antonyvenis1212@gmail.com'
-# EMAIL_HOST_PASSWORD = 'ilew ihtg wfhg ascg'
-EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+EMAIL_HOST_USER = 'antonyvenis1212@gmail.com'
+EMAIL_HOST_PASSWORD = 'ilew ihtg wfhg ascg'
+# EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+# EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 
 ADMIN_SITE_HEADER = "🔥 Antony Admin Panel"
 ADMIN_SITE_TITLE = "Dashboard"
