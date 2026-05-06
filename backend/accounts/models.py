9d130f69a95ccfd3235ed68e92f8bc9661cc57ce
antonyvenis <antonyvenis1212@gmail.com>
2026-05-06 17:18:26 +0530

    Images-Upload-Error-Clear

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils import timezone
+import uuid
 
 
 # ============================
@@ -113,6 +114,13 @@ class OrderItem(models.Model):
         return f"{self.item_name} x {self.quantity}"
 
 
+
+def product_image_path(instance, filename):
+    ext = filename.split('.')[-1]
+    new_name = f"{uuid.uuid4()}.{ext}"
+    return f"products/{new_name}"
+
+
 # ============================
 # 🍔 PRODUCT MODEL
 # ============================
@@ -122,11 +130,12 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     category = models.CharField(max_length=100)
     rating = models.FloatField(default=0)
-    image = models.ImageField(upload_to="products/", null=True, blank=True)
+    image = models.ImageField(upload_to=product_image_path, null=True, blank=True)
     stock = models.IntegerField(default=10)
     offer = models.IntegerField(default=0)  # % discount
     is_active = models.BooleanField(default=True)
 
     def __str__(self):
         return self.name
-      
+
+
