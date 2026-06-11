f72e83d6c23a03c1267c3aaa3aa024c1df53f0dd
antonyvenis <antonyvenis1212@gmail.com>
2026-06-11 22:39:49 +0530

    Create SuperUser AdminPanel

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1443,6 +1443,8 @@ from reportlab.platypus import SimpleDocTemplate, Table, TableStyle, Paragraph, 
 from reportlab.lib.units import inch
 from django.http import HttpResponse
 import datetime
+from django.contrib.auth import get_user_model
+
 
 
 # ================================
@@ -2203,6 +2205,24 @@ def test_email(request):
         fail_silently=False,
     )
     return Response({"message": "Email sent"})
+
+from django.contrib.auth import get_user_model
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+@api_view(["GET"])
+def create_admin(request):
+    User = get_user_model()
+
+    if not User.objects.filter(username="admin").exists():
+        User.objects.create_superuser(
+            username="antony",
+            email="antonyvenis1212@gmail.com",
+            password="Venisking@419"
+        )
+
+    return Response({"message": "Admin created"})
+
 
 
 # ================================
