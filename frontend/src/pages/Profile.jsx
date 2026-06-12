f3eac7913f9703d54da59e678d8c533c90bdb087
antonyvenis <antonyvenis1212@gmail.com>
2026-06-12 15:11:40 +0530

    NEW Updated

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -219,9 +219,7 @@ function Profile() {
 
           <button>
             <Link to="/about" id="Link-text">Aboutℹ️</Link>
-          </button><br></br>
-
-          <button><Link to="/herobanner" id="Link-text">⚡👑⚡</Link></button>
+          </button>
 
           {/* 📊 Stats */}
           <div className="stats">
