703ed3132f7708bb46c575d3e66002d308c3852e
antonyvenis <antonyvenis1212@gmail.com>
2026-06-13 15:47:12 +0530

    Brevo SMTP Reconnect Updated

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -183,9 +183,9 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 EMAIL_HOST = 'smtp-relay.brevo.com'
 
-EMAIL_PORT = 587
+# EMAIL_PORT = 587
 
-EMAIL_USE_TLS = True
+# EMAIL_USE_TLS = True
 
 EMAIL_HOST_USER = 'antonyvenis1212@gmail.com'
 
