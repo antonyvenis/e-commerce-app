ecfecea60a02c84386b9bb4795e366d3adf870b2
antonyvenis <antonyvenis1212@gmail.com>
2026-06-15 16:00:49 +0530

    Render Deploy Error Clear!

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -184,7 +184,6 @@ from .views import (
     verify_register_otp,
     test_email,
     generate_invoice,
-    create_admin,
 )
 
 urlpatterns = [
@@ -212,5 +211,4 @@ urlpatterns = [
     path("products/", get_products),
     path("load-products/", load_products),
     path("add-products-bulk/", add_products_bulk),
-    path("create-admin/", create_admin),
 ]
