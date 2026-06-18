a1f28cc4b41f617dfdcd7e89f16cfb1d2ec9c051
antonyvenis <antonyvenis1212@gmail.com>
2026-06-19 01:22:44 +0530

    UPDATE NEW!

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1224,15 +1224,12 @@ def generate_invoice(request, order_id):
     doc.build(elements)
     return response
 
-from django.contrib.auth import get_user_model
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
 
 @api_view(["GET"])
 def create_admin(request):
     User = get_user_model()
 
-    if not User.objects.filter(username="antony").exists():
+    if not User.objects.filter(username="admin").exists():
         User.objects.create_superuser(
             username="antony",
             email="antonyvenis1212@gmail.com",
