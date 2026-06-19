5885ad3b8aa25b3937dc0e6214f4c7e0a1f9106a
antonyvenis <antonyvenis1212@gmail.com>
2026-06-19 09:02:03 +0530

    UPDATE NEW!

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -184,7 +184,6 @@ from .views import (
     verify_register_otp,
     test_email,
     generate_invoice,
-    create_admin,
     health_check,
 )
 
@@ -213,6 +212,5 @@ urlpatterns = [
     path("products/", get_products),
     path("load-products/", load_products),
     path("add-products-bulk/", add_products_bulk),
-    path("create-admin/", create_admin),
     path("health/", health_check)
 ]
